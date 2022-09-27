34e2bcc71a848125969f3215ebe7e57df05466e2
Alain Noel <alain.noel34@gmail.com>
2022-09-27 21:08:59 +0200

    reset button ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { PlayPauseButton } from "./PlayPauseButton";
 import { Display } from "./Display";
 import { PointScoredButton } from "./PointScoredButton";
+import { ResetButton } from "./ResetButton";
 
 export default function App() {
   return (
@@ -10,7 +11,10 @@ export default function App() {
           <PointScoredButton playerId="player1">Point Joueur 1</PointScoredButton>
           <PointScoredButton playerId="player2">Point joueur 2</PointScoredButton>
         </div>
-        <PlayPauseButton />
+        <div className="buttons-row">
+          <ResetButton />
+          <PlayPauseButton />
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import { store} from "./store";
-import { StrictMode} from "react";
-
+import { store } from "./store";
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Provider store= {store}>
+const root = createRoot(rootElement);
+root.render(
+  <Provider store={store}>
     <StrictMode>
       <App />
     </StrictMode>
   </Provider>,
-  rootElement
 );
