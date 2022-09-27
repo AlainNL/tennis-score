4bdf5fe2c176616f54672b93ede94c08419752f1
Alain Noel <alain.noel34@gmail.com>
2022-09-27 17:30:43 +0200

    add provider on app.js

--- src/PlayPauseButton.js
+++ src/PlayPauseButton.js
@@ -3,7 +3,12 @@ import { useDispatch } from "react-redux";
 export function PlayPauseButton() {
   const dispatch =  useDispatch();
 
-  return <button onClick={() => {
-  dispatch({ type: "playPause" })
-  }} >Pause /Reprendre</button>
+  return (
+    <button onClick={() => {
+      dispatch({ type: "playPause" })
+      }}
+      >
+        Pause /Reprendre
+      </button>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import { store} from "./store";
-import { StrictMode } from 'react';
+import { StrictMode} from "react";
 
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Provider store={store}>
-      <StrictMode>
-          <App />
-      </StrictMode>
+  <Provider store= {store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Provider>,
   rootElement
 );
